2c40a55f6403d52d786a9dff0276c074c155ae2c
Nikola Saratlija <1005923@hr.nl>
2022-11-20 17:50:46 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import PriceFilter from "./components/ProductFilters/PriceFilter";
 function App() {
     return (
         <div className="lg:container lg:mx-auto lg:px-8">
-            <section className="m-2 lg:m-4 lg:p-4 lg:border-2 flex flex-col gap-y-4">
+            <section className="m-2 lg:m-4 lg:p-4 lg:border-2 flex flex-col gap-y-4 rounded-md">
                 <ProductFilter name={"Category"}/>
                 <PriceFilter/>
             </section>
 
-            <section className="m-2 lg:m-4 lg:p-4 lg:border-2">
+            <section className="m-2 lg:m-4 lg:p-4 lg:border-2 rounded-md">
                 <ProductContainer/>
             </section>
         </div>
--- src/components/ProductFilters/PriceFilter.jsx
+++ src/components/ProductFilters/PriceFilter.jsx
@@ -5,7 +5,15 @@ export const PriceFilter = () => (
         <span className="text-orange-600 font-bold mb-2">Price Range</span>
 
         <span className="font-bold uppercase mb-1 text-xs">€12 - €1323</span>
-        <RangeSlider colorScheme='orange' w={"33%"} aria-label={['min', 'max']} defaultValue={[10, 30]}>
+        <RangeSlider
+            colorScheme='orange'
+            w={"33%"}
+            aria-label={['min', 'max']}
+            min={12}
+            max={1323}
+            defaultValue={[12, 1323]}
+            onChangeEnd={val => console.log(val)}
+        >
             <RangeSliderTrack>
                 <RangeSliderFilledTrack/>
             </RangeSliderTrack>
